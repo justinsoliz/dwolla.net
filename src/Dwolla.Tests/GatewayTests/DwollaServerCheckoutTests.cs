5f07b74d3becaaa9c8d5d049e4e844197835a7c1
Justin Soliz <justinsoliz@gmail.com>
2012-11-27 23:05:49 -0800

    adds guest checkout with off-site gateway

---
+++ src/Dwolla.Tests/GatewayTests/DwollaServerCheckoutTests.cs
@@ -0,0 +1,54 @@
+﻿using Dwolla.Gateway;
+using Dwolla.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Dwolla.Tests.GatewayTests
+{
+    [TestFixture]
+    public class DwollaServerCheckoutTests
+    {
+        [Test]
+        public void server_checkout_api_can_verify_callback()
+        {
+            //appSecret = "test"
+            var c = new DwollaCallback {
+                Amount = 3.25m,
+                CheckoutId = "C3D4DC4F-5074-44CA-8639-B679D0A70803",
+                Signature = "7f42ba58ff0d20486fdc2634745e8e7c92cb6321"
+            };
+
+            var api = new DwollaServerCheckoutApi("test", "test");
+
+            api.VerifyCallbackAuthenticity(c)
+                .ShouldBeTrue();
+        }
+
+        [Test]
+        public void server_checkout_api_can_get_redirect_url_on_successful_response()
+        {
+            var r = new DwollaCheckoutResponse {
+                Result = DwollaCheckoutResponseResult.Success,
+                CheckoutId = "C3D4DC4F-5074-44CA-8639-B679D0A70803",
+            };
+
+            var api = new DwollaServerCheckoutApi("test", "test");
+
+            var redirectUrl = api.GetCheckoutRedirectUrl(r);
+
+            redirectUrl.ShouldEqual("https://www.dwolla.com/payment/checkout/C3D4DC4F-5074-44CA-8639-B679D0A70803");
+        }
+
+        [Test]
+        public void getting_redirect_url_from_failed_checkout_response_throws_exception()
+        {
+            var r = new DwollaCheckoutResponse {
+                Result = DwollaCheckoutResponseResult.Failure,
+                Message = "invalid total."
+            };
+
+            var api = new DwollaServerCheckoutApi("test", "test");
+            //new Action( () => api.GetCheckoutRedirectUrl( r ) )
+            //    .ShouldThrow<ValidationException>();
+        }
+    }
+}
